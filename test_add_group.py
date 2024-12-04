38497bb1cf1ee22a263af82852570bf73ab247d6
iDaDan <tamemask@gmail.com>
2024-12-04 20:44:58 +0300

    Class name corrected to Py style

--- test_add_group.py
+++ test_add_group.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
-class TestAddGroup(unittest.TestCase):
+class test_name_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(30)
