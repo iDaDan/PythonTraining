276b6312c4bea3b4e9ed02ee9fc05a85a1533621
iDaDan <tamemask@gmail.com>
2024-12-07 21:05:02 +0300

    User class added

--- test_add_user.py
+++ test_add_user.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from login import Login
+from user import User
 import unittest
 
 
@@ -17,75 +18,78 @@ class test_name_user(unittest.TestCase):
         self.open_home_page(wd)
         self.make_login(wd, Login(username="admin", password="secret"))
         # add to separate method/class
-        self.create_user(self, wd, self.fill_element_by_name)
-        wd.find_element("link text", "home").click()
+        self.create_user(wd, User(firstname = "Thor", middlename = "Ivanov", lastname = "Odinovich", nickname = "GodOfThunder",
+                                  title = "What is Title", company = "Asgard", address = "Still Asgard", home = "Bifrust", mobile = "Sacrifice", work = "Hammer Management", fax = "Axe", email = "thor@asgard.ru", homepage = "thor.asgard.ru", bday = "5", bmonth = "October", byear = "1000",
+                                  aday = "6", amonth = "November", ayear = "1001"))
+        self.return_home_page(wd)
         self.logout(wd)
-''' will be debugged later
+
+    def return_home_page(self, wd):
+        wd.find_element("link text", "home").click()
+
+    # will be debugged later
     def fill_element_by_name(self, wd, indicator, text):
         self.text = text
         self.indicator = indicator
         wd.find_element("name", indicator).click()
         wd.find_element("name", indicator).clear()
         wd.find_element("name", indicator).send_keys(text)
-'''
-    def create_user(self, wd):
+
+    def create_user(self, wd, user):
         wd.find_element("link text", "add new").click()
-        #fill_element_by_name(self, wd, "firstname", "Thor")
+        # fill_element_by_name(self, wd, "firstname", "Thor")
         wd.find_element("name", "firstname").click()
         wd.find_element("name", "firstname").clear()
-        wd.find_element("name", "firstname").send_keys("Tor")
+        wd.find_element("name", "firstname").send_keys(user.firstname)
         wd.find_element("name", "middlename").click()
         wd.find_element("name", "middlename").clear()
-        wd.find_element("name", "middlename").send_keys("Ivanov")
+        wd.find_element("name", "middlename").send_keys(user.middlename)
         wd.find_element("name", "lastname").click()
         wd.find_element("name", "lastname").clear()
-        wd.find_element("name", "lastname").send_keys("Odinovich")
+        wd.find_element("name", "lastname").send_keys(user.lastname)
         wd.find_element("name", "nickname").click()
         wd.find_element("name", "nickname").clear()
-        wd.find_element("name", "nickname").send_keys("GodOfThunder")
-        wd.find_element("name", "theform").click()
-        wd.find_element("name", "nickname").click()
-        wd.find_element("name", "nickname").click()
+        wd.find_element("name", "nickname").send_keys(user.nickname)
         wd.find_element("name", "title").click()
         wd.find_element("name", "title").clear()
-        wd.find_element("name", "title").send_keys("What is Title")
+        wd.find_element("name", "title").send_keys(user.title)
         wd.find_element("name", "company").click()
         wd.find_element("name", "company").clear()
-        wd.find_element("name", "company").send_keys("Asgard")
+        wd.find_element("name", "company").send_keys(user.company)
         wd.find_element("name", "address").click()
         wd.find_element("name", "address").clear()
-        wd.find_element("name", "address").send_keys("Still Asgard")
+        wd.find_element("name", "address").send_keys(user.address)
         wd.find_element("name", "home").click()
         wd.find_element("name", "home").clear()
-        wd.find_element("name", "home").send_keys("Bifrust")
+        wd.find_element("name", "home").send_keys(user.home)
         wd.find_element("name", "mobile").clear()
-        wd.find_element("name", "mobile").send_keys("Sacrifice")
+        wd.find_element("name", "mobile").send_keys(user.mobile)
         wd.find_element("name", "work").click()
         wd.find_element("name", "work").clear()
-        wd.find_element("name", "work").send_keys("Hammer Management")
+        wd.find_element("name", "work").send_keys(user.work)
         wd.find_element("name", "fax").click()
         wd.find_element("name", "fax").clear()
-        wd.find_element("name", "fax").send_keys("Axe")
+        wd.find_element("name", "fax").send_keys(user.fax)
         wd.find_element("name", "email").click()
         wd.find_element("name", "email").clear()
-        wd.find_element("name", "email").send_keys("thor@asgard.ru")
+        wd.find_element("name", "email").send_keys(user.email)
         wd.find_element("name", "homepage").click()
         wd.find_element("name", "homepage").clear()
-        wd.find_element("name", "homepage").send_keys("thor.asgard.ru")
+        wd.find_element("name", "homepage").send_keys(user.homepage)
         wd.find_element("name", "bday").click()
-        Select(wd.find_element("name", "bday")).select_by_visible_text("16")
+        Select(wd.find_element("name", "bday")).select_by_visible_text(user.bday)
         wd.find_element("name", "bmonth").click()
-        Select(wd.find_element("name", "bmonth")).select_by_visible_text("October")
+        Select(wd.find_element("name", "bmonth")).select_by_visible_text(user.bmonth)
         wd.find_element("name", "byear").click()
         wd.find_element("name", "byear").clear()
-        wd.find_element("name", "byear").send_keys("1000")
+        wd.find_element("name", "byear").send_keys(user.byear)
         wd.find_element("name", "aday").click()
-        Select(wd.find_element("name", "aday")).select_by_visible_text("17")
+        Select(wd.find_element("name", "aday")).select_by_visible_text(user.aday)
         wd.find_element("name", "amonth").click()
-        Select(wd.find_element("name", "amonth")).select_by_visible_text("November")
+        Select(wd.find_element("name", "amonth")).select_by_visible_text(user.amonth)
         wd.find_element("name", "ayear").click()
         wd.find_element("name", "ayear").clear()
-        wd.find_element("name", "ayear").send_keys("1001")
+        wd.find_element("name", "ayear").send_keys(user.ayear)
         wd.find_element("xpath", "//div[@id='content']/form/input[20]").click()
 
     def open_home_page(self, wd):
---
+++ user.py
@@ -0,0 +1,23 @@
+class User:
+    
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, 
+                 home, mobile, work, fax, email, homepage, bday, bmonth, byear, aday, amonth, ayear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
