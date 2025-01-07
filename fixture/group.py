c1832687c88742d6c274dfc40dd08af450239a87
idadan <d9093211188@gmail.com>
2025-01-07 18:55:26 +0300

    Выполнена Оптимизация ввода данных в формы

--- fixture/group.py
+++ fixture/group.py
@@ -25,27 +25,37 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         #select_first_group
-        wd.find_element("name", "selected[]").click()
+        self.select_first_group()
         #sibmit deletion
         wd.find_element("name", "delete").click()
 
-    def modify(self, group):
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        wd.find_element("name", "selected[]").click()
+        self.select_first_group()
+        #open modification form
         wd.find_element("name", "edit").click()
         # fill group firm
-        self.fill_group_info(group)
-        # submit group creation
+        self.fill_group_info(new_group_data)
+        # submit group modification
         wd.find_element("name", "update").click()
         self.return_group_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element("name", "selected[]").click()
+
     def fill_group_info(self, group):
         wd = self.app.wd
-        wd.find_element("name", "group_name").click()
-        wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys(group.name)
-        wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys(group.header)
-        wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys(group.footer)
+        self.insert_group_info("group_name", group.name)
+        self.insert_group_info("group_header", group.header)
+        self.insert_group_info("group_footer", group.footer)
+
+    def insert_group_info(self, find_name, info_text):
+        wd = self.app.wd
+        if info_text is not None:
+            wd.find_element("name", find_name).click()
+            wd.find_element("name", find_name).clear()
+            wd.find_element("name", find_name).send_keys(info_text)
+        else:
+            pass
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -1,7 +1,12 @@
 from model.group import Group
 from login import Login
 
-def test_add_group(app):
+def test_mod_first_group(app):
     app.session.make_login(Login(username="admin", password="secret"))
-    app.group.modify(Group(name="TestX", header="TestY", footer="TestW"))
+    app.group.modify_first_group(Group(name="TestX", header="TestY", footer="TestW"))
+    app.session.logout()
+
+def test_mod_name_first_group(app):
+    app.session.make_login(Login(username="admin", password="secret"))
+    app.group.modify_first_group(Group(name="NaMeNaMe"))
     app.session.logout()
